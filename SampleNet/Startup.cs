56bb3a51de43db164f27aa6baada4a5afa37bbb6
Pavel Zeman <pavel@pavel.cz>
2017-07-15 00:53:52 +0200

    Initial commit

---
+++ SampleNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleNet.Startup))]
+namespace SampleNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
